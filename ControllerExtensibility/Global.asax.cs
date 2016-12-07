b40104d12362302cda153f1cf959266512d2ddc1
Preetham K H <blighter.soul@gmail.com>
2016-12-07 16:34:32 +1100

    initial commit

---
+++ ControllerExtensibility/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //ControllerBuilder.Current.SetControllerFactory(new CustomerControllerFactory());
+            ControllerBuilder.Current.SetControllerFactory(new DefaultControllerFactory(new CustomControllerActivator()));
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyControllerNamespace");
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyProject.*");
+        }
+    }
+}
---
+++ ControllerExtensibility/Infrastructure/CustomControllerActivator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Controllers;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class CustomControllerActivator : IControllerActivator // demonstrates how you can use the IControllerActivator interface to intercept requests between the controller factory and dependency resolver
+    {
+        public IController Create(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == typeof(ProductController))
+            {
+                controllerType = typeof(CustomerController);
+            }
+
+            return (IController) DependencyResolver.Current.GetService(controllerType);
+        }
+    }
+}
